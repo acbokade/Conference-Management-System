1066fce9a542b488724c34acd6aeb6a286cb44d9
ajinkya <acbokade@gmail.com>
2021-04-05 23:14:18 +0530

    minor fixes

--- cms/accounts/models.py
+++ cms/accounts/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 
 class User(models.Model):
+    # char field ?
     email = models.TextField(null=False, primary_key=True)
     password = models.TextField()
     name = models.TextField()
