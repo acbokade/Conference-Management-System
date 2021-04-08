2332e52bed66356805c3b4185364854ea42f4294
chauhanjatin10 <cs17btech11019@iith.ac.in>
2021-04-08 11:38:04 +0530

    templates resolved

--- cms/accounts/models.py
+++ cms/accounts/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 
 
 class User(models.Model):
-    # char field ?
     email = models.TextField(null=False, primary_key=True)
     password = models.TextField()
     name = models.TextField()
