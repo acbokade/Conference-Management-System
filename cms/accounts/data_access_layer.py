bfd30fb00a83cd4b8ded92ce8624947a36378b5a
chauhanjatin10 <cs17btech11019@iith.ac.in>
2021-04-16 10:38:39 +0530

    forgot password made via security question and answer

--- cms/accounts/data_access_layer.py
+++ cms/accounts/data_access_layer.py
@@ -8,5 +8,14 @@ def obtain_user_by_email(email):
 def obtain_research_profile(email):
     return models.ResearchProfile.objects.get(person=obtain_user_by_email(email))
 
-def get_user_by_username(uname):
+
+def get_user_by_username(name):
     return models.User.objects.get(name=name)
+
+
+def obtain_user_security_question(email):
+    return models.SecurityQuestions.objects.get(user=obtain_user_by_email(email)).question
+
+
+def obtain_user_security_answer(email):
+    return models.SecurityQuestions.objects.get(user=obtain_user_by_email(email)).answer
