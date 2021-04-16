f3cbeed4661f149e0627f890708729b6672609c0
chauhanjatin10 <cs17btech11019@iith.ac.in>
2021-04-16 11:43:27 +0530

    AC display button

--- cms/conference/utils.py
+++ cms/conference/utils.py
@@ -1,5 +1,6 @@
 from .models import Conference
 from . import data_access_layer as conf_dao
+from reviewer import models as reviewer_models
 
 
 def get_subject_areas_list_by_conf_name(conf_name):
@@ -7,3 +8,25 @@ def get_subject_areas_list_by_conf_name(conf_name):
     subject_areas = conf.subject_areas
     subject_areas_list = subject_areas.split(',')
     return subject_areas_list
+
+
+def obtain_ca_boolean_array(user_email, confs_list):
+    arr = []
+    for conf in confs_list:
+        conf_ca_list = [em.strip() for em in conf.ca_emails.split(' ')]
+        if user_email.strip() in conf_ca_list:
+            arr.append(True)
+        else:
+            arr.append(False)
+    return arr
+
+
+def obtain_invited_rev_boolean_array(user_email, confs_list):
+    arr = []
+    for conf in confs_list:
+        try:
+            reviewer_models.InvitedReviewers.objects.get(user=user_email, conference=conf)
+            arr.append(True)
+        except:
+            arr.append(False)
+    return arr
